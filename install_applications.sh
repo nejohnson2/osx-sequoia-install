e2554352c4b87da3dca6bccd1be5e0185c25f258
Nicholas Johnson <nejohnson2@users.noreply.github.com>
2025-08-07 11:48:43 -0400

    Update install_applications.sh

--- install_applications.sh
+++ install_applications.sh
@@ -14,3 +14,14 @@ brew install --cask google-chrome
 brew install --cask lastpass
 brew install --cask chatgpt
 brew install --cask visual-studio-code
+
+# Configure Github
+git config --global user.name "Nicholas Johnson"
+git config --global user.email "nejohnson2@gmail.com"
+
+# do this manually
+#ssh-keygen -t ed25519 -C "nejohnson2@gmail.com"
+#eval "$(ssh-agent -s)"
+#ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+# copy your key to github
+# pbcopy < ~/.ssh/id_ed25519.pub
