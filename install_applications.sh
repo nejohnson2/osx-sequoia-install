ee648d9235fba52256bd16281040c55d41e4d6f6
Nicholas Johnson <nejohnson2@users.noreply.github.com>
2025-08-06 23:24:54 -0400

    Update install_applications.sh

--- install_applications.sh
+++ install_applications.sh
@@ -2,3 +2,7 @@
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Add homebrew to path
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
